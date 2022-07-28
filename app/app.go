0d0d8745395e9842a08e2d567e5a2499c2146321
da-maltsev <danil.zlatoust9999@gmail.com>
2022-07-28 16:08:17 +0500

    GetBook fix

--- app/app.go
+++ app/app.go
@@ -2,7 +2,6 @@ package app
 
 import (
 	"github.com/da-maltsev/UDV_Go_test/app/handler"
-	"github.com/da-maltsev/UDV_Go_test/config"
 	"github.com/da-maltsev/UDV_Go_test/model"
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
@@ -15,15 +14,9 @@ type App struct {
 	DB     *gorm.DB
 }
 
-func (a *App) Initialize(config *config.Config) {
-	//dbURI := fmt.Sprintf("%s:%s@/%s?charset=%s&parseTime=True",
-	//	config.DB.Username,
-	//	config.DB.Password,
-	//	config.DB.Name,
-	//	config.DB.Charset)
+func (a *App) Initialize() {
 
 	db, err := gorm.Open("postgres", "host=localhost port=5432  user=postgres password=postgres dbname=booksdb ")
-	//db, err := gorm.Open(config.DB.Dialect, dbURI)
 	if err != nil {
 		log.Fatalf("Could not connect database: %s", err)
 	}
--- main.go
+++ main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"github.com/da-maltsev/UDV_Go_test/app"
-	"github.com/da-maltsev/UDV_Go_test/config"
 )
 
 func main() {
-	config := config.GetConfig()
 
 	app := &app.App{}
-	app.Initialize(config)
-	app.Run(":3000")
+	app.Initialize()
+	app.Run(":3001")
 }
