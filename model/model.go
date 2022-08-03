e00f0238a842880ff54c29852e9901da84de7783
da-maltsev <danil.zlatoust9999@gmail.com>
2022-08-03 14:56:14 +0500

    model fix

--- model/model.go
+++ model/model.go
@@ -7,50 +7,51 @@ import (
 )
 
 type Publisher struct {
-	gorm.Model
+	ID       int    `gorm:"primary_key, AUTO_INCREMENT" json:"id"`
 	Title    string `json:"title"`
 	Address  string `json:"address"`
 	Contacts string `json:"contacts"`
 }
 
 type Book struct {
-	Id          int       `gorm:"primary_key, AUTO_INCREMENT" json:"id"`
+	ID          int       `gorm:"primary_key, AUTO_INCREMENT" json:"id"`
 	Title       string    `json:"title"`
 	Description string    `json:"description"`
 	Authors     string    `json:"authors"`
 	PublishYear time.Time `json:"publishYear"`
 	PublisherID int       `json:"publisher_id"`
-	Publisher   Publisher
+	Publisher   Publisher `json:"publisher"`
 	Edition     string    `json:"edition"`
 }
 
 type Item struct {
-	Id       int `gorm:"primary_key, AUTO_INCREMENT" json:"id"`
-	Quantity int `json:"quantity"`
-	BookID   int `json:"book_id"`
-	Book     Book
+	ID       int  `gorm:"primary_key, AUTO_INCREMENT" json:"id"`
+	Quantity int  `json:"quantity"`
+	BookID   int  `json:"book_id"`
+	Book     Book `json:"book"`
 }
 
 type Position struct {
-	gorm.Model
+	ID     int    `gorm:"primary_key, AUTO_INCREMENT" json:"id"`
 	Room   string `json:"room"`
 	Shelf  string `json:"shelf"`
 	ItemID int    `json:"item_id"`
-	Item   Item
+	Item   Item   `json:"item"`
 }
 
 type Customer struct {
-	gorm.Model
+	ID       int    `gorm:"primary_key, AUTO_INCREMENT" json:"id"`
 	Name     string `json:"name"`
 	Contacts string `json:"contacts"`
 }
 
 type Status struct {
-	gorm.Model
+	ID         int       `gorm:"primary_key, AUTO_INCREMENT" json:"id"`
 	ItemID     int       `json:"item_id"`
+	Item       Item      `json:"item"`
 	Available  bool      `json:"available"`
 	CustomerID int       `json:"customer_id"`
-	Customer   Customer
+	Customer   Customer  `json:"customer"`
 	GivenAt    time.Time `json:"given_at"`
 	ReceivedAt time.Time `json:"received_at"`
 }
